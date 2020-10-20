446c88bcebbbc03a7cbd2f248be72a0e8fcd2ab0
Harmanjit Singh <er.harmanjitsingh@gmail.com>
2020-10-20 06:27:22 -0400

    main

--- src/app/Manufacturer/manu-list/manu-list.component.ts
+++ src/app/Manufacturer/manu-list/manu-list.component.ts
@@ -1,3 +1,4 @@
+import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +8,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManuListComponent implements OnInit {
 
+  priceOfJeans = [2,3];
+  priceOfShoes = [4];
+  priceOfSkirt = [2,3];
+  priceOfTop = [1,2];
+  count: number = 0;
+
+  budget: number;
+
+  calculate(max)
+  {
+    var budget = max.budgetAmount;
+    for(var index in this.priceOfJeans)
+    {
+      var Jean = this.priceOfJeans[index];
+      console.log("Jean Price is " +Jean);
+
+      for( var index in this.priceOfSkirt)
+      {
+        var Skirt = this.priceOfSkirt[index];
+        console.log( "Skirt Price is " +Skirt);
+        
+        for( var index in this.priceOfTop)
+        {
+          var Top = this.priceOfTop[index];
+          console.log("Top Price is " +Top);
+
+          var total = Jean + Skirt + Top;
+          console.log("Total Price is " +total);
+                
+          if( total <= budget)
+          {
+            this.count++;
+            console.log("Count is: " +this.count)
+            console.log("Jean are " +Jean + " Skirt are " +Skirt + "Top are " +Top)
+          }
+        }
+      }
+    }
+  }
   constructor() { }
 
   ngOnInit(): void {
